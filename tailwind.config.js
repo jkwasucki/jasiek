01953bc0d04a88039b809545d3202789f2319aac
azherid <kwakwasucki@wp.pl>
2024-10-16 22:53:02 +0200

    S

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: [
+    './public/**/*.html',
+    './public/**/*.js',],
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./Components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./out/**/*.{html,js}"
   ],
   theme: {
     extend: {},
